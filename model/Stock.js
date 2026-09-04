c64faec999ef8ad502ea5ca5dd8d249fbb7ff813
Saotome <orlandocookie12@gmail.com>
2026-09-04 19:57:53 +0630

    finish stock model in mongodb

--- model/Stock.js
+++ model/Stock.js
@@ -1,2 +1,34 @@
+const mongoose = require("mongoose");
 
+const stockSchema = new mongoose.Schema({
+    company:{
+        type:  String,
+        required: true
+    },
+    description:{
+        type:  String,
+        required: true
+    },
+    intital_price: {
+        type: Number,
+        required: true
+    },
+    price_2002: {
+        type: Number,
+        required: true
+    },
+    price_2007: {
+        type: Number,
+        required: true
+    },
+    symbol:{
+        type:  String,
+        required: true
+    }
+
+}, {timestamps: true});
+
+
+
+module.exports = mongoose.model("Stock",stockSchema);
 
